e081d043370680761ac3d0f8e9992f98b2b5dbab
Argel Hernandez <argel6767@gmail.com>
2024-07-23 16:12:21 -0400

    initalizes repository

---
+++ backend/peakperformance_backend/src/main/java/com/peakperformace/peakperformance_backend/PeakperformanceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.peakperformace.peakperformance_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeakperformanceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeakperformanceBackendApplication.class, args);
+	}
+
+}
---
+++ backend/peakperformance_backend/src/test/java/com/peakperformace/peakperformance_backend/PeakperformanceBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peakperformace.peakperformance_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeakperformanceBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
