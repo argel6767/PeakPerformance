e7c13a5b2e9295fee552e300fe6c485941c7c5c5
Argel Hernandez <argel6767@gmail.com>
2024-07-25 10:23:28 -0400

    creates UserTes

---
+++ backend/peakperformance_backend/src/test/java/com/peakperformace/peakperformance_backend/user/UserTest.java
@@ -0,0 +1,12 @@
+package com.peakperformace.peakperformance_backend.user;
+
+import org.junit.jupiter.api.Test;
+
+
+public class UserTest {
+
+    @Test
+    void makeUserWithBothCurrentLiftsAndGoals() {
+        
+    }
+}
