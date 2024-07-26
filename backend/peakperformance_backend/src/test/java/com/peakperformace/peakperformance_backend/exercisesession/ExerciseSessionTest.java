3023430474f0ca60c29735fd9ab8370a1a324ac6
unknown <tomdion35@gmail.com>
2024-07-25 20:55:46 -0400

    add ExerciseSession tests

--- backend/peakperformance_backend/src/test/java/com/peakperformace/peakperformance_backend/exercisesession/ExerciseSessionTest.java
+++ backend/peakperformance_backend/src/test/java/com/peakperformace/peakperformance_backend/exercisesession/ExerciseSessionTest.java
@@ -1,5 +1,34 @@
 package com.peakperformace.peakperformance_backend.exercisesession;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.peakperformace.peakperformance_backend.exercise.Exercise;
+import com.peakperformace.peakperformance_backend.exercise.model.WeightReps;
+import com.peakperformace.peakperformance_backend.exercise.model.WorkoutType;
+
 public class ExerciseSessionTest {
     
+    //make a set of a workout
+    WeightReps latPulldown = new WeightReps(160, 10);
+    List<WeightReps> set1 = List.of(latPulldown);
+
+    //make a exercise
+    Exercise lateralPulldown = new Exercise("Lateral Pulldown", WorkoutType.BACK);
+
+    @Test
+    void testExerciseSessionCreation(){
+
+    ExerciseSession exerciseSession = new ExerciseSession(LocalTime.of(5,10), LocalDate.of(2024, 6, 18), set1, lateralPulldown);
+    assertEquals(LocalTime.of(5,10), exerciseSession.getTimeOfExercise());
+    assertEquals(LocalDate.of(2024, 6, 18), exerciseSession.getDateOfExercise());
+    assertEquals(set1, exerciseSession.getSets());
+    assertEquals(lateralPulldown, exerciseSession.getExercise());
+
+    }
 }
