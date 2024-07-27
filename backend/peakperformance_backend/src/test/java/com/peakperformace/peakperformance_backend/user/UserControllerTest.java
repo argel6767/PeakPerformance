8c471ccb199e0099faf85c03f860f29d1d55a99b
Argel Hernandez <argel6767@gmail.com>
2024-07-26 21:52:22 -0400

    creates UserControllerTest

---
+++ backend/peakperformance_backend/src/test/java/com/peakperformace/peakperformance_backend/user/UserControllerTest.java
@@ -0,0 +1,45 @@
+package com.peakperformace.peakperformance_backend.user;
+
+import org.junit.jupiter.api.Test;
+
+public class UserControllerTest {
+    @Test
+    void testAddLiftById() {
+
+    }
+
+    @Test
+    void testDeleteUserById() {
+
+    }
+
+    @Test
+    void testGetUserByEmail() {
+
+    }
+
+    @Test
+    void testGetUserById() {
+
+    }
+
+    @Test
+    void testGetUserWeightById() {
+
+    }
+
+    @Test
+    void testGetUsers() {
+
+    }
+
+    @Test
+    void testUpdateCurrentLiftsOfUserById() {
+
+    }
+
+    @Test
+    void testUpdateWeightOfUserById() {
+
+    }
+}
