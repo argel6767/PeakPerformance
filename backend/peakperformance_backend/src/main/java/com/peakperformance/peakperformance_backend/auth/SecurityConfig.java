dba5eb2cf89c5a56d0bd67c17ebbbb0b90aede15
argel6767 <argel6767@gmail.com>
2024-07-30 17:17:36 -0400

    securityFilterChain

--- backend/peakperformance_backend/src/main/java/com/peakperformance/peakperformance_backend/auth/SecurityConfig.java
+++ backend/peakperformance_backend/src/main/java/com/peakperformance/peakperformance_backend/auth/SecurityConfig.java
@@ -1,5 +1,31 @@
 package com.peakperformance.peakperformance_backend.auth;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable()) // Disable CSRF as tokens are used
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/public/").permitAll() // 
+                .anyRequest().authenticated() // Secure all other endpoints
+            )
+            .sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Stateless sessions
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(withDefaults()) // Use JWT for OAuth2 Resource Server
+            );
+
+        return http.build();
+    }
 
 }
