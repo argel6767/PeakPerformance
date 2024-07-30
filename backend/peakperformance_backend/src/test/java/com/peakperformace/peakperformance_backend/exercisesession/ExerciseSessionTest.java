f9da01c3e9beae632e4edbc0ef1197f8332dbf9f
unknown <tomdion35@gmail.com>
2024-07-30 19:18:11 -0400

    Fixes Tests for ExerciseSession

--- backend/peakperformance_backend/src/test/java/com/peakperformace/peakperformance_backend/exercisesession/ExerciseSessionTest.java
+++ backend/peakperformance_backend/src/test/java/com/peakperformace/peakperformance_backend/exercisesession/ExerciseSessionTest.java
@@ -1,15 +1,16 @@
 package com.peakperformace.peakperformance_backend.exercisesession;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import com.peakperformace.peakperformance_backend.exercise.Exercise;
 import com.peakperformace.peakperformance_backend.exercise.model.WeightReps;
 import com.peakperformace.peakperformance_backend.exercise.model.WorkoutType;
+import com.peakperformace.peakperformance_backend.user.User;
 
 public class ExerciseSessionTest {
     
@@ -20,13 +21,16 @@ public class ExerciseSessionTest {
     //make a exercise
     Exercise lateralPulldown = new Exercise("Lateral Pulldown", WorkoutType.BACK);
 
+    //make a user
+    User user1 = new User("Jerry", "Roberts", "Jrob10@gmail.com", "HelloWorld", LocalDate.of(2006, 03, 30), 5, 205);
+
     @Test
     void testExerciseSessionCreation(){
 
-    ExerciseSession exerciseSession = new ExerciseSession(LocalDateTime.of(2024, 06, 05, 5, 20), set1, lateralPulldown);
+    ExerciseSession exerciseSession = new ExerciseSession(LocalDateTime.of(2024, 06, 05, 5, 20), set1, lateralPulldown, user1);
     assertEquals(LocalDateTime.of(2024, 06,  05, 5, 20), exerciseSession.getDateTimeofExercise());
     assertEquals(set1, exerciseSession.getSets());
     assertEquals(lateralPulldown, exerciseSession.getExercise());
-
+    assertEquals(user1, exerciseSession.getUser());
     }
 }
