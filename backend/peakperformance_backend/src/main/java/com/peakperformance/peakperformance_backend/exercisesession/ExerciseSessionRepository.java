c96d7b961b0cec2c74573040657cfaf8f6ddbfa4
argel6767 <argel6767@gmail.com>
2024-08-07 19:48:50 -0400

    removes Exercise entity

--- backend/peakperformance_backend/src/main/java/com/peakperformance/peakperformance_backend/exercisesession/ExerciseSessionRepository.java
+++ backend/peakperformance_backend/src/main/java/com/peakperformance/peakperformance_backend/exercisesession/ExerciseSessionRepository.java
@@ -6,20 +6,16 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import com.peakperformance.peakperformance_backend.exercise.Exercise;
 import com.peakperformance.peakperformance_backend.user.User;
 
 
-public interface ExerciseSessionRepository extends JpaRepository<ExerciseSession, LocalDateTime> {
+public interface ExerciseSessionRepository extends JpaRepository<ExerciseSession, Long> {
 
     //finds an exercise session by datetime
     @Query("SELECT es FROM ExerciseSession es WHERE es.dateTimeofExercise = :dateTimeOfExerciseSession")
     public ExerciseSession findByDateTimeofExerciseSession(LocalDateTime dateTimeOfExerciseSession);
 
-    //finds an exercise session by exercise
-    @Query("SELECT es FROM ExerciseSession es WHERE es.exercise = :exercise")
-    public ExerciseSession findByExercise(Exercise exercise);
-
+ 
     //finds exercise session by user
     @Query("SELECT es FROM ExerciseSession es WHERE es.user = ?1")
     List<ExerciseSession> findByUser(User user);
