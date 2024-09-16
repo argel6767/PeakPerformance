b18e3fa67dc03becb83ccaee912c6f96610642bd
Argel Hernandez <argel6767@gmail.com>
2024-09-15 20:05:48 -0400

    adds mock dependency injection to UserControllerTest

--- backend/peakperformance_backend/src/test/java/com/peakperformance/peakperformance_backend/user/UserControllerTest.java
+++ backend/peakperformance_backend/src/test/java/com/peakperformance/peakperformance_backend/user/UserControllerTest.java
@@ -1,11 +1,19 @@
 package com.peakperformance.peakperformance_backend.user;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
 
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService; // Mock dependency
     @Test
     void testAddLiftById() {
 
