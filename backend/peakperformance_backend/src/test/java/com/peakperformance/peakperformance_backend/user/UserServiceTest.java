ba1f70f21eec06ead34b335b482961c622e6d12c
Argel Hernandez <argel6767@gmail.com>
2024-09-15 22:24:00 -0400

    creates tests for UserServiceTest

--- backend/peakperformance_backend/src/test/java/com/peakperformance/peakperformance_backend/user/UserServiceTest.java
+++ backend/peakperformance_backend/src/test/java/com/peakperformance/peakperformance_backend/user/UserServiceTest.java
@@ -1,16 +1,28 @@
 package com.peakperformance.peakperformance_backend.user;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.peakperformance.peakperformance_backend.user.UserService.UserNotFoundException;
+
 //TODO FINSIH THESE ONCE ExerciseSession is merged!!!!!
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest
 public class UserServiceTest {
 
     @InjectMocks
@@ -24,43 +36,56 @@ public class UserServiceTest {
     @BeforeEach
     void init() {
         user = new User();
+        user.setId(1L);
+        user.setEmail("example@email.com");
+        user.setWeight(23);
     }
 
     @Test
     void testDeleteUserById() {
-
+        Long userId = 1L;
+        Mockito.doNothing().when(userRepository).deleteById(userId);
+        userService.deleteUserById(userId);
+        Mockito.verify(userRepository, Mockito.times(1)).deleteById(userId);
     }
 
     @Test
     void testGetAllUsers() {
-
+        
     }
 
     @Test
-    void testGetUserByEmail() {
-
+    void testGetUserByEmailWithValidEmail() throws UserNotFoundException {
+        when(userRepository.findUserByEmail(user.getEmail())).thenReturn(Optional.of(user));
+        User foundUser = userService.getUserByEmail(user.getEmail());
+        assertTrue(foundUser != null);
+        assertTrue(foundUser instanceof User);
+        assertEquals(user.getEmail(), foundUser.getEmail());
     }
-    /*@Test
-    public void testGetUserById() throws UserNotFoundException {
-        Long userId = 1L;
-        user.setId(userId);
-        user.setEmail("test@example.com");
 
-        Mockito.when(userRepository.findById(userId)).thenReturn(Optional.of(user));
-
-        User foundUser = userService.getUserById(userId);
-
+    @Test
+    void testGetUserByEmailWithInvalidEmailThrowsUserNotFoundException() {
+        when(userRepository.findUserByEmail("InvalidEmail@email.com")).thenReturn(Optional.empty());
+        assertThrows(UserNotFoundException.class, () -> {
+            userService.getUserByEmail("InvalidEmail@email.com");
+        }, "UserNotFoundException should have been thrown!");
+        verify(userRepository, times(1)).findUserByEmail("InvalidEmail@email.com");
+    }
+    @Test
+    public void testGetUserByIdWithValidId() throws UserNotFoundException {
+        Mockito.when(userRepository.findById(user.getId())).thenReturn(Optional.of(user));
+        User foundUser = userService.getUserById(user.getId());
         assertNotNull(foundUser);
-
-        assertEquals(userId, foundUser.getId());
-        assertEquals("test@example.com", foundUser.getEmail());
-    }*/
+        assertEquals(user.getId(), foundUser.getId());
+        assertEquals("example@email.com", foundUser.getEmail());
+    }
 
     @Test
-    void testGetUserByIdThrowsUserNotFoundException() {
-        user.setId(Long.valueOf(2));
-
-        //Mockito.when(userService.getUserById(null))
+    void testGetUserByIdWithInvalidIdThrowsUserNotFoundException() {
+        when(userRepository.findById(2L)).thenReturn(Optional.empty());
+        assertThrows(UserNotFoundException.class, () -> {
+            userService.getUserById(2L);
+        }, "UserNotFoundException should have been thrown!");
 
     }
 
