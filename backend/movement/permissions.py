6a669e5e37277e78b3da97223e987e5ab3103e95
argel6767 <argel6767@gmail.com>
2025-04-11 14:50:27 -0400

    feat: create Views for Movement and Muscle

--- backend/movement/permissions.py
+++ backend/movement/permissions.py
@@ -3,6 +3,6 @@ from rest_framework import permissions
 '''
 only allows requests by admin level users
 '''
-class isAdminUser(permissions.BasePermission):
-    def has_permission(self, request):
+class IsAdminUser(permissions.BasePermission):
+    def has_permission(self, request, view):
         return bool(request.user and request.user.is_staff)
--- backend/movement/views.py
+++ backend/movement/views.py
@@ -1,3 +1,24 @@
-
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
+from models import Muscle, Movement
+from .permissions import IsAdminUser
+from .serializers import MuscleSerializer, MovementSerializer
 
 # Create your views here.
+"""
+API view to create a new muscle entry.
+Only admin users can create new muscles.
+"""
+class MuscleViewSet(ModelViewSet):
+    serializer_class = MuscleSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    queryset = Muscle.objects.all()
+
+"""
+API view to create a new movement entry.
+Only admin users can create new movements.
+"""
+class MovementViewSet(ModelViewSet):
+    serializer_class = MovementSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    queryset = Movement.objects.all()
