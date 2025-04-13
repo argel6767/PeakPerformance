02aa6a9800ee174319acb4013e825b10b7bee2e8
Argel Hernandez <argel6767@gmail.com>
2025-04-13 14:55:43 -0400

    refactor: update scripts with determine_environment

--- backend/scripts/run_tests.py
+++ backend/scripts/run_tests.py
@@ -2,13 +2,13 @@ import subprocess
 from pathlib import Path
 import platform
 from sys import exit
-from build_image import load_env_files, delete_old_image_and_container, build_image
+from build_image import load_env_files, delete_old_image_and_container, build_image, determine_environment
 
 '''
 Loads env variables into terminal before building image, then running tests inside using docker-compose
 '''
 cd = Path.cwd()
-docker_path = cd/'docker' if platform.system() != 'Linux' else cd/'backend'/'docker' # checking if this is a pipeline being ran, since its a linux machine
+docker_path = None # checking if this is a pipeline being ran, since its a linux machine
 is_os_windows = platform.system() == 'Windows' #windows needs shell otherwise permissions errors will not let script run processes
 
 def run_test_suite():
@@ -18,6 +18,7 @@ def run_test_suite():
     return process.returncode
 
 def main():
+    determine_environment()
     load_env_files()
     delete_old_image_and_container()
     build_image()
