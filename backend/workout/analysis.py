ca53037d9bcbcffa54579acc22dd89fc54106093
argel6767 <argel6767@gmail.com>
2025-04-19 00:23:09 -0400

    test: write tests for calculations under analysis.py

--- backend/workout/analysis.py
+++ backend/workout/analysis.py
@@ -1,4 +1,4 @@
-from models import Set
+from workout.models import Set
 from users.models import UserWeight
 
 '''
@@ -9,14 +9,14 @@ This file contains functions for various workout and progress related calculatio
 def calculate_total_weight_volume(sets: list[Set]) -> float:
     total_volume= 0
     for set in sets:
-        total_volume += set.reps * set.weight
+        total_volume += (set.reps * set.weight)
     return total_volume
 
 # Calculates the Progressive Overload Rate between two weeks to determine progress
 def calculate_progress_overload_rate(initial_total_volume:float, current_total_volume) -> float:
     return (current_total_volume - initial_total_volume)/initial_total_volume
 
-EPLEY_CONSTANT = 0.0333
+EPLEY_CONSTANT = 1/30 #.0333...
 
 # Calculates Epley Formula, ie a user's estimated one rep max
 def calculate_epley_formula(top_set:Set) -> float:
@@ -26,4 +26,4 @@ def calculate_epley_formula(top_set:Set) -> float:
 
 # Calculates a users estimated relative strength based off their 1RM and Body weight
 def calculate_relative_strength(one_rep_max:float, userWeight:UserWeight) -> float:
-    return one_rep_max/userWeight
+    return one_rep_max/userWeight.weight
