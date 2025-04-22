370e20e3cbf6a0ac1ea7b7fe14c3f301333b1fe6
Argel Hernandez <argel6767@gmail.com>
2025-04-22 18:09:01 -0400

    feat: write Model's APIViews

--- backend/workout/views.py
+++ backend/workout/views.py
@@ -1,5 +1,5 @@
-from users.models import CustomUser
-from .serializers import WorkoutDtoSerializer
+from .serializers import WorkoutDtoSerializer, WorkoutExerciseDtoSerializer, SetDtoSerializer
+from .models import Workout, WorkoutExercise, Set
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -14,4 +14,31 @@ class WorkoutAPIView(APIView):
         if (not serializer.is_Valid()):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+        user = request.user
+        workout = Workout.createWorkoutEntry(email=user, workout_dto=serializer)
+        
+        return Response({'success', workout}, status.HTTP_201_CREATED)
+
+class WorkoutExerciseAPIView(APIView):
     
+    def post(self, request):
+        serializer = WorkoutExerciseDtoSerializer(request.data, many=True)
+        
+        if (not serializer.is_Valid()):
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        exercises = WorkoutExercise.createWorkoutExerciseEntries(workout_exercises_dto=serializer)
+        
+        return Response({'success', exercises}, status.HTTP_201_CREATED)
+    
+class SetAPIView(APIView):
+    
+    def post(self, request):
+        serializer = SetDtoSerializer(request.data, many=True)
+        
+        if (not serializer.is_Valid()):
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+        sets = Set.createSetEntries(sets_dto=serializer)
+
+        return Response({'success', sets}, status=status.HTTP_201_CREATED)
