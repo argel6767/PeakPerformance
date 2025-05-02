758c7328f2aa016527757c63b541b506f2a01f28
argel6767 <argel6767@gmail.com>
2025-05-02 00:46:23 -0400

    fix: fix stale container issue

--- backend/analysis/urls.py
+++ backend/analysis/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 
 urlpatterns = [
     path('progressive-overload/', views.get_progressive_overload, name='get-progressive-overload'),
-    path('orm/<int:movement_id>/', views.get_orm, name='get-orm')
+    path('orm/<int:movement_id>/', views.get_orm, name='get-one-rep-max')
 ]
